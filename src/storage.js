70a7024092fc67e15309033757d99d78980bb95b
David Čáp <capdavidd@gmail.com>
2024-10-20 21:46:06 +0200

    Add uuid to background storage script, update pages

--- src/storage.js
+++ src/storage.js
@@ -3,20 +3,14 @@ import uuid from 'uuid/v4';
 /* global chrome */
 export const getData = () => {
     return new Promise((resolve, reject) => {
-        chrome.storage.sync.get(['items'], function(result) {
+        chrome.storage.sync.get(['items'], function (result) {
             if (chrome.runtime.lastError) {
                 console.error(chrome.runtime.lastError.message);
                 reject(chrome.runtime.lastError.message);
             } else {
-                let res = result.items ? result.items : [{ itemName: '', number: 0, id: uuid() }];
+                let res =
+                    result.items && result.items.length ? result.items : [{ itemName: '', number: 0, id: uuid() }];
 
-                res = res.length ? res : [{ itemName: '', number: 0, id: uuid() }];
-
-                res.forEach(item => {
-                    item.id = item.id ? item.id : uuid();
-                });
-
-                storageSync(res);
                 resolve(res);
             }
         });
@@ -24,6 +18,6 @@ export const getData = () => {
 };
 
 export const storageSync = items =>
-    chrome.storage.sync.set({ items }, function() {
+    chrome.storage.sync.set({ items }, function () {
         console.log('storage synced', items);
     });
